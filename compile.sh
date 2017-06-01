c6b97bbef96151e530bb204487ef71e61ef16692
v <eng.vitoria.souza@gmail.com>
2017-06-01 03:01:36 -0300

    minha primeira edicao

---
+++ compile.sh
@@ -0,0 +1,11 @@
+latex VADS_GREEN_FIWI
+bibtex VADS_GREEN_FIWI
+latex VADS_GREEN_FIWI
+latex VADS_GREEN_FIWI
+
+pdflatex VADS_GREEN_FIWI
+bibtex VADS_GREEN_FIWI
+pdflatex VADS_GREEN_FIWI
+pdflatex VADS_GREEN_FIWI
+
+rm *.aux *.log *.blf *.bbl *.blg *.out
